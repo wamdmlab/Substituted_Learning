9a0b2440d7875dcade5f08e8ff7b374ab8c4af67
pengyingtao <63253716+pengyingtao@users.noreply.github.com>
2022-07-31 16:01:06 +0800

    Create model.py

---
+++ 数据学习/baseline/code/model.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestRegressor  
+import sys
+from sklearn.metrics import f1_score
+import jieba
+
+class model():
+    def __init__(self,train_features , train_label,modelname='SVM'):
+        self.train_features=train_features
+        self.train_label=train_label
+        self.modelname=modelname
+        self.mymodel=None
+        if modelname=='SVM':
+            self.mymodel=SVC(kernel= "rbf")#kernel：参数选择有rbf, linear, poly, Sigmoid, 默认的是"RBF";ß
+        elif modelname=='randomforest':
+            self.mymodel=RandomForestRegressor()
+    def train(self):
+        print('开始训练...')
+        self.mymodel.fit(self.train_features,self.train_label)
+    def predict(self,valid_features):
+        preds=self.mymodel.predict(valid_features)
+        preds=[np.round(lb) for lb in preds ]
+        return preds
