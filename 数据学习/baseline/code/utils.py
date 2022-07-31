f86def88b80b9816e8e9258083e857affebd977f
pengyingtao <63253716+pengyingtao@users.noreply.github.com>
2022-07-31 16:01:27 +0800

    Create utils.py

---
+++ 数据学习/baseline/code/utils.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from sklearn.feature_extraction.text import TfidfVectorizer
+import jieba
+
+def segmentWord(contents): 
+    ret = [] 
+    for sent in contents:
+        word_list = list(jieba.cut(sent))
+        combine_words = " ".join(word_list)
+        ret.append(combine_words) 
+    return ret
+
+
+def readData(train ,valid, test):
+    print("训练样本 = %d" % len(train))
+    print("验证样本 = %d" % len(valid))
+    print("测试样本 = %d" %len(test))
+    content_train=segmentWord(train['content'])
+    content_valid=segmentWord(valid['content'])
+    content_test=segmentWord(test['content'])
+
+    vectorizer = TfidfVectorizer(analyzer='word',min_df=3,token_pattern=r"(?u)\b\w\w+\b")
+    train_features = vectorizer.fit_transform(content_train)
+    print("训练样本特征表长度为 %s"%str(train_features.shape))
+    # print(vectorizer.get_feature_names()) #特征名展示
+    valid_features = vectorizer.transform(content_valid)
+    print("验证样本特征表长度为 %s"%str(valid_features.shape))
+    test_features = vectorizer.transform(content_test)
+    print("测试样本特征表长度为 %s"%str(test_features.shape))
+    data=[train,valid,test,train_features,valid_features,test_features]
+    return data
